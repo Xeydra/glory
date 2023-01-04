15566750568d6b8fa95be844fbec9fad1a7c8d65
Xeydra <annmarix@gmail.com>
2023-01-04 14:02:23 +0100

    contact

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import Index from '@/pages/index.vue';
 import Collection from '@/pages/collection.vue';
 import About from '@/pages/about.vue';
+import Contact from '@/pages/contact.vue';
 
 export var BASE_URL = 'http://localhost:1337';
 
@@ -11,6 +12,7 @@ export var NAVIGATION_URL = API_URL + 'navigation/render/1?type=TREE';
 export var HOMEPAGE_URL = API_URL + 'homepage?populate[collection][populate][image][populate]=*';
 export var FOOTER_URL = API_URL + 'footer/';
 export var ABOUT_URL = API_URL + 'about-pages/';
+export var CONTACT_URL = API_URL + 'contact/?populate[blocks][populate][details]=*';
 
 export function getOneCollectionURL(id) {
   return COLLECTION_URL + id + '?populate[widgets][populate][media_img_slider][populate]=*' +
@@ -28,4 +30,5 @@ export var COMP_TYPES = [
   { key: 'homepage', component: Index },
   { key: 'collection', component: Collection },
   { key: 'about-page', component: About },
+  { key: 'contact', component: Contact },
 ]
